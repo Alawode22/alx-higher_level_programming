3a234f6a80f233a1cd4e540ffe55521003b5043f
Alawode22 <attaurrahmanalawode@gmail.com>
2023-03-14 10:42:07 +0000

    task3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for b in range(97, 123)
-    if (b = 101 or b = 113)
+for b in range(97, 123):
+    if (b = 101 or b = 113):
         continue
     print("{}".format(chr(a)), end="")
