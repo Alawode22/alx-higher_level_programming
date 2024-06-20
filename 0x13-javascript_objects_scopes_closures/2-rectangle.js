b2751fc6a7e5c674ebffacd4b95d59afc069de31
Alawode22 <attaurrahmanalawode@gmail.com>
2024-06-20 03:21:02 -0700

    task 2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (height <= 0 || width <= 0) {
+      return {};
+    }
+    this.width = h;
+    this.height = h;
+  }
+};
