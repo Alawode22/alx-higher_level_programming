39f32c138844472df56e3dbb8ff4f38b7985f0d6
Alawode22 <attaurrahmanalawode@gmail.com>
2024-06-19 21:34:31 -0700

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let arg1 = parseInt(process.argv[2]);
-let arg2 = parseInt(process.argv[3]);
+const arg1 = parseInt(process.argv[2]);
+const arg2 = parseInt(process.argv[3]);
 if (arg1 === undefined || isNaN(arg1)) {
   console.log('NaN');
 } else {
