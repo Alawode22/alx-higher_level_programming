9aff13d304826ea81d3f9611e76edd36b104c26a
Alawode22 <attaurrahmanalawode@gmail.com>
2024-06-20 21:00:48 -0700

    task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const list = require('./100-data.js').list;
-const map = list.map(x);
+const map = list.map(x => x);
 const map2 = list.map((x, index) => x * index);
 console.log(map);
 console.log(map2);
