0262b86e7a23542bbd8053699fe215aa21702bcf
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-10 14:54:18 -0700

    task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
     # Check if c is a single lowercase character
-    return isinstance(c, str) and len(c) == 1 and 'a' <= c <= 'z'
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
