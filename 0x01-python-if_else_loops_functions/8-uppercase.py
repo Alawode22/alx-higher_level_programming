3e972fb4ea7d95838eb1e96e7c72096e44eb792e
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-19 01:40:09 -0700

    task 8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    new_str = []
+    for char in str:
+        if 'a' <= char <= 'z':
+            upp_char = chr(ord(char) - 32)
+        else:
+            upp_char = char
+        new_str.append(upp_char)
+    upp_str = ''.join(new_str)
+    print(upp_str)
