d0658c2ddbcc16b27d97b6b9579dd36da865ef14
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-27 02:47:55 -0700

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for row in matrix:
-            for i, value in enumerate(row):
-                if i < len(row) - 1:
-                    print("{:d}".format(value ** 2), end=' ')
-                else:
-                    print("{:d}".format(value ** 2), end='')
-            print()
-    else:
-        return
+def square_matrix_simple(matrix=[]):
+    new matrix = [
+        [element ** 2 for element in row]
+        for row in matrix
+    ]
+    return new_matrix
