a97e0d91b76d2d605aa0e2b7e6c41efe0a679658
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-30 15:43:35 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,6 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 
-fs.readFile('filePath', 'utf8', (err, data) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
+fs.readFile('filePath', 'utf8', (err, content) => {
+  console.log(err || content);
 });
