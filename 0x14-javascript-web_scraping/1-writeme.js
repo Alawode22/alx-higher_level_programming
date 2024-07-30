fde6af3b192d76519c7b845d8873e568a74a7308
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-30 15:57:33 -0700

    task 0

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,4 +5,8 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(filePath, string, 'utf8');
+fs.writeFile(filePath, string, 'utf8', (error) => {
+  if (error) {
+    console.error(error);
+  }
+});
