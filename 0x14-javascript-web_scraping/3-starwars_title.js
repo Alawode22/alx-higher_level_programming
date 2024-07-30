a946c4ed0db24118b388a3db14d4002073c6e2a5
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-30 16:25:09 -0700

    task 0

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,15 @@ const request = require('request');
 const movieId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error: Status code', response.statusCode);
+    return;
+  }
   const film = JSON.parse(body);
   console.log(film.title);
 });
