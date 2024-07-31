bef4b021b98b60bb12f300389c9c3bb76914328d
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 02:25:28 -0700

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,8 @@
+-- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT title
+FROM tv_shows
+LEFT JOIN tv_genres ON tv_genres.id = tv_shows.id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+GROUP BY title
+ORDER BY title ASC;
