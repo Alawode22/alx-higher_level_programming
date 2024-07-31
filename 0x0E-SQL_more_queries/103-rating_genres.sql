c6f52f688a53d62e251dfbe8b83f5ac8b8f223e2
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 04:48:05 -0700

    task 103

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,7 +2,7 @@
 -- sql
 SELECT tv_genres.name AS genre, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
 GROUP BY tv_genres.name
 ORDER BY rating DESC;
