a55889d1108049d81372cbdd0a49eab6d3e9b693
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-30 21:40:25 -0700

    task 101

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,49 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+if (!movieId) {
+  console.error('Usage: ./101-starwars_characters.js <Movie-ID>');
+  process.exit(1);
+}
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Error: Status code', response.statusCode);
+    process.exit(1);
+  }
+
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  printCharacters(characters, 0);
+});
+
+function printCharacters(characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], (charError, charResponse, charBody) => {
+    if (charError) {
+      console.error(charError);
+      return;
+    }
+
+    if (charResponse.statusCode !== 200) {
+      console.error('Error: Status code', charResponse.statusCode);
+      return;
+    }
+
+    const characterDetails = JSON.parse(charBody);
+    console.log(characterDetails.name);
+    printCharacters(characters, index + 1);
+  });
+}
