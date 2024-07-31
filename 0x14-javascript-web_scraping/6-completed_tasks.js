8351b0a77154640894b129dbfa5d4e65958acbfb
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-30 18:33:59 -0700

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error('Code: ', response.statusCode);
+    process.exit(1);
+  }
+
+  const tasks = JSON.parse(body);
+});
+const completedTasks = {};
+tasks.forEach(task => {
+  if (task.completed) {
+    if (completedTasks[task.userId]) {
+      completedTasks[task.userId]++;
+    } else {
+      completedTasks[task.userId] = 1;
+    }
+  }
+});
