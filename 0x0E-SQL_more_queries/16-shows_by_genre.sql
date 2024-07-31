c61d0223bf0ed8f1e8117ffb472da479b97bca96
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 02:34:37 -0700

    task 15

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,5 @@
 --Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+-- lists all rows of a table linked to another table
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
