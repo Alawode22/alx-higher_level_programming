e94405363c1fc33fb4ee3370aaf775688083b6e5
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 04:31:23 -0700

    task 101

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -2,11 +2,11 @@
 -- uses a subquery
 SELECT title
 FROM tv_shows
-WHERE id NOT IN (
+WHERE title NOT IN (
     SELECT title
     FROM tv_shows
     LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
     LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
     WHERE tv_genres.name = 'Comedy')
 GROUP BY title
-ORDER BY title ASC;
+ORDER BY title ASCi;
