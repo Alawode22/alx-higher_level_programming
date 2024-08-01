754e246a506fdea5391d1a909f952888506b55d9
Alawode22 <attaurrahmanalawode@gmail.com>
2024-07-31 18:00:42 -0700

    task 102

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     # Collect keys to delete
-    keys_to_delete = [key for key in a_dictionary if a_dictionary[key] == value]
+    keys_to_delete = [
+        key for key in a_dictionary if a_dictionary[key] == value
+    ]
 
     # Delete each key from the dictionary
     for key in keys_to_delete:
