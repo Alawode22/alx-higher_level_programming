30e3f3909586808ca722aad59f4985a2ac463de4
Alawode22 <attaurrahmanalawode@gmail.com>
2024-08-09 14:05:05 -0700

    square-size

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+defines an empty class
+"""
+
+
+class Square:
+    """a square class with errors caught"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+        def area(self):
+            """Return the area of the square"""
+            return self.__size ** 2
