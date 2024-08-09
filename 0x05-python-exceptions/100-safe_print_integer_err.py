78d0c62305665f062f18d3827caad0ebc84f30ac
Alawode22 <attaurrahmanalawode@gmail.com>
2024-08-09 04:09:42 -0700

    div-error

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -2,7 +2,7 @@
 
 def safe_print_integer_err(value):
     try:
-        print("{}".format(value))
+        print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as err:
         print(f"Exception: {e}", file=sys.stderr)
